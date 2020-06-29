e3557d19c8aacc3da933c283032e5eebcd2ec5cc
kevin fernandez <kevinfernandezsoftdev@gmail.com>
2020-06-28 22:30:46 -0400

    Updates

---
+++ precache-manifest.e1aa259151e6849655813adda4322bef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58f0346507b517edd83640003df32428",
+    "url": "/jibaro/index.html"
+  },
+  {
+    "revision": "ad20a703c82df3fb323c",
+    "url": "/jibaro/static/css/main.7872c039.chunk.css"
+  },
+  {
+    "revision": "a67fda7487e21f21b321",
+    "url": "/jibaro/static/js/2.6211fe80.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/jibaro/static/js/2.6211fe80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad20a703c82df3fb323c",
+    "url": "/jibaro/static/js/main.82e5a6f7.chunk.js"
+  },
+  {
+    "revision": "49430e950181533305ec",
+    "url": "/jibaro/static/js/runtime-main.d9de535b.js"
+  }
+]);
